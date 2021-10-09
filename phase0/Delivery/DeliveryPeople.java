c5efe725fd815680d8bc5743afd0137ada6ff394
y-zhaoyu <90292905+y-zhaoyu@users.noreply.github.com>
2021-10-09 12:12:16 -0400

    Update DeliveryPeople.java

--- phase0/Delivery/DeliveryPeople.java
+++ phase0/Delivery/DeliveryPeople.java
@@ -5,14 +5,14 @@ public class DeliveryPeople implements User{
         private String name;
         private int number;
         private String email;
-        private String delivery_area;
+        private String deliveryArea;
         private LocalTime time;
 
         public customer (String name, int number, String email, String area) {
             this.name = name;
             this.number = number;
             this.email = email;
-            this.delivery_area = area;
+            this.deliveryArea = area;
             this.time = LocalTime.now();
         }
     }
