dc42cfdb0fedefe1ca1ae58e52438844f175cacd
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-10-09 12:19:12 -0400

    copy paste mistake

--- phase0/Delivery/DeliveryPeople.java
+++ phase0/Delivery/DeliveryPeople.java
@@ -1,19 +1,17 @@
-public class DeliveryPeople implements User{
-    import java.time;
+import java.time;
 
-    public class delivery_person implements User {
-        private String name;
-        private int number;
-        private String email;
-        private String delivery_area;
-        private LocalTime time;
+public class delivery_person implements User {
+    private String name;
+    private int number;
+    private String email;
+    private String delivery_area;
+    private LocalTime time;
 
-        public customer (String name, int number, String email, String area) {
-            this.name = name;
-            this.number = number;
-            this.email = email;
-            this.delivery_area = area;
-            this.time = LocalTime.now();
-        }
+    public customer (String name, int number, String email, String area) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.delivery_area = area;
+        this.time = LocalTime.now();
     }
 }
