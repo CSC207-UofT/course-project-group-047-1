0f2b3cfcdaefec4408292a14449a50ee5a237454
wenzhenw <90291967+wenzhenw@users.noreply.github.com>
2021-10-09 12:24:02 -0400

    Update GroceryItem.java

--- phase0/Delivery/GroceryItem.java
+++ phase0/Delivery/GroceryItem.java
@@ -1,13 +1,11 @@
 public class GroceryItem{
-    public class grocery_items {
-        private String item_name;
-        private double price;
-        private int quantity_in_stock;
+    private String item_name;
+    private double price;
+    private int quantity_in_stock;
 
-        public grocery_items (String name, int price, int quantity) {
-            this.item_name = name;
-            this.price = price;
-            this.quantity_in_stock = quantity;
-        }
+    public grocery_items (String name, int price, int quantity) {
+        this.item_name = name;
+        this.price = price;
+        this.quantity_in_stock = quantity;
     }
 }
