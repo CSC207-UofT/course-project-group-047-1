18d1aba926c3504d760f6e1ccd0f8585a4b192a8
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-10-09 12:08:46 -0400

    entities

--- phase0/Delivery/Customer.java
+++ phase0/Delivery/Customer.java
@@ -1,1 +1,17 @@
-public class Customer implements User{}
+public class Customer implements User{
+    public class customer implements User {
+        private String name;
+        private int number;
+        private String email;
+        private String paymentInfo;
+        private String address
+
+        public customer (String name, int number, String email, String paymentInfo, String address) {
+            this.name = name;
+            this.number = number;
+            this.email = email;
+            this.paymentInfo = paymentInfo;
+            this.address = address;
+        }
+    }
+}
--- phase0/Delivery/DeliveryPeople.java
+++ phase0/Delivery/DeliveryPeople.java
@@ -1,1 +1,19 @@
-public class DeliveryPeople implements User{}
+public class DeliveryPeople implements User{
+    import java.time;
+
+    public class delivery_person implements User {
+        private String name;
+        private int number;
+        private String email;
+        private String delivery_area;
+        private LocalTime time;
+
+        public customer (String name, int number, String email, String area) {
+            this.name = name;
+            this.number = number;
+            this.email = email;
+            this.delivery_area = area;
+            this.time = LocalTime.now();
+        }
+    }
+}
