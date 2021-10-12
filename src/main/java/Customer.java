7f421345f64f033d51ab908fe3b813c25351c13a
YICONG LUO <90354531+Ratbuyer@users.noreply.github.com>
2021-10-12 03:38:08 -0400

    Add files via upload

---
+++ src/main/java/Customer.java
@@ -0,0 +1,15 @@
+public class Customer implements User {
+    private String name;
+    private int number;
+    private String email;
+    private String paymentInfo;
+    private String address
+
+    public Customer (String name, int number, String email, String paymentInfo, String address) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.paymentInfo = paymentInfo;
+        this.address = address;
+    }
+}
