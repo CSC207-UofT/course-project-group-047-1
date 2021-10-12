063ea84b9c8337571a3ddc57c6ab6bdae96d75a6
khloet <khloe.tsang@mail.utoronto.ca>
2021-10-12 19:00:01 -0400

    Modified AccountManager.java
    This is incomplete! Will try to fix asap but I won't be able to fix until Thursday :(

--- phase0/Delivery/AccountManager.java
+++ phase0/Delivery/AccountManager.java
@@ -1,1 +1,18 @@
-public class AccountManager{}
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountManager {
+    private Map<Integer, String> accounts;
+
+    public AccountManager() {
+        accounts = new HashMap<>();
+    }
+
+    public void storeUserName(String user, int number) {
+        accounts.put(number, user);
+    }
+
+    public String getUserName(int number) {
+        return accounts.get(number);   // this doesn't look right
+    }
+}
