c1817b03cd798a5f15cf3b894879c6a6441f73a2
candicelu <candicecl.lu@mail.utoronto.ca>
2021-10-13 22:13:56 -0400

    delete user interface and change customer and delivery people

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,1 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class AccountTest {
+
+    Customer customer;
+
+    @Before
+    public void setUp() {customer = new Customer("username", 12345,
+            "123@utoronto.ca", "address");}
+
+    @Test(timeout = 50)
+    public void testGetUsername() {assertEquals("username", customer.getUsername());}
+
+    @Test(timeout = 50)
+    public void testGetPin() {assertEquals(12345, customer.getpin());}
+
+}
