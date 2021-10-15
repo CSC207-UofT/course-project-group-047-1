3491f3286fdb0349d26a3fbf0b3dd160b2192f36
Ratseller <yicong.luo@mail.utoronto.ca>
2021-10-14 20:11:01 -0400

    code structure change

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,26 +1,45 @@
 import java.util.ArrayList;
 
+
 public class ShoppingCart {
+
+
     private ArrayList<GroceryItem> items;
 
+
     public ShoppingCart() {
-        this.items = new ArrayList<GroceryItem>();
+
+        this.items = new ArrayList<>();
+
     }
+
 
     public void AddItem(GroceryItem item) {
+
         this.items.add(item);
+
     }
+
 
     public void RemoveItem(int index) {
+
         this.items.remove(index);
+
     }
 
+
     public double getTotalPrice() {
+
         int total = 0;
+
         for (GroceryItem i: this.items) {
+
             total += (i.getPrice())*(i.getQuantity());
+
         }
+
         return total;
+
     }
 
 
