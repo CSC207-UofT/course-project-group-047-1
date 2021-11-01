ae0421f278509dd82b51ad4d089dbe6b63bc7f92
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-01 19:42:30 -0400

    added id variable to GroceryItem and function to update inventory for later when order completion is implemented

--- src/main/java/Use_Case/GroceryInventory.java
+++ src/main/java/Use_Case/GroceryInventory.java
@@ -3,25 +3,23 @@ package Use_Case;
 
 import Entities.GroceryItem;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class GroceryInventory {
 
+    String path = System.getProperty("user.dir") + File.separator + "Inventory.txt";
     private final ArrayList<GroceryItem> items = new ArrayList<>();
 
     public GroceryInventory() {
 
-        String path = System.getProperty("user.dir") + File.separator + "Inventory.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                int price = Integer.parseInt(reader.readLine());
+                String name = reader.readLine();
+                double price = Double.parseDouble(reader.readLine());
                 int quantity = Integer.parseInt(reader.readLine());
-                GroceryItem g = new GroceryItem(line, price, quantity);
+                GroceryItem g = new GroceryItem(Integer.parseInt(line), name, price, quantity);
                 this.items.add(g);
             }
         } catch (IOException e) {
@@ -30,6 +28,22 @@ public class GroceryInventory {
 
     }
 
+    public void UpdateInventory() {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            for (GroceryItem item : this.items) {
+                writer.write(item.getId());
+                writer.newLine();
+                writer.write(item.getName());
+                writer.newLine();
+                writer.write(String.valueOf(item.getPrice()));
+                writer.newLine();
+                writer.write(item.getQuantity());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public ArrayList<GroceryItem> getItems() {
         return items;
     }
