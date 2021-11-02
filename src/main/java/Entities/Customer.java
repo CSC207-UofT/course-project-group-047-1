8504b33ac66e788192d7f0a6736939a71d71fed3
Ariel <ariel.xu@mail.utoronto.ca>
2021-11-01 23:33:43 -0400

    Customer and Account

--- src/main/java/Entities/Customer.java
+++ src/main/java/Entities/Customer.java
@@ -1,44 +1,17 @@
 package Entities;
 
-
-import Controllers.ShoppingCart;
-
-public class Customer {
+//this class stores the information of a customer
+public class Customer{
 
 
-    private final String username;
-    private final int pin;
-    private final String email;
-    private final String address;
-    private final ShoppingCart cart;
+    private final String name;
 
 
-    public Customer (String username, int pin, String email, String address) {
-
-        this.username = username;
-        this.pin = pin;
-        this.email = email;
-        this.address = address;
-        this.cart = new ShoppingCart();
-
-    }
+    //constructor
+    public Customer (String name){this.name = name;}
 
 
-    public Customer (String username, int pin) {
-
-        this.username = username;
-        this.pin = pin;
-        this.address = "";
-        this.email = "";
-        this.cart = new ShoppingCart();
-
-    }
-
-
-    public String getUsername() {return this.username;}
-
-
-    public int getPin() {return this.pin;}
-
+    //a helper method
+    public String getName(){return this.name;}
 
 }
