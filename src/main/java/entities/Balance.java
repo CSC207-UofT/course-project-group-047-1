9ed74439450c948497b056fc49a07addb45a3f59
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-25 15:45:54 -0500

    added a Balance class. constructor initiates balance at 0

---
+++ src/main/java/entities/Balance.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class Balance {
+
+    private int bal;
+
+    /**
+     * Constructor
+     * Initiates a balance with value 0
+     */
+    public Balance() {
+        this.bal = 0;
+    }
+
+}
