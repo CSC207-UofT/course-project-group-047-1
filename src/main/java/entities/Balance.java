ee30469ea36b212bced5a633c0c8562c97f5e157
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-25 16:01:13 -0500

    Added methods: addBal: adds a given amount to the balance removeBal: removes a given amount from the balance only if the resulting balance is non-negative getBal: returns the current balance

--- src/main/java/entities/Balance.java
+++ src/main/java/entities/Balance.java
@@ -2,7 +2,7 @@ package entities;
 
 public class Balance {
 
-    private int bal;
+    private double bal;
 
     /**
      * Constructor
@@ -12,7 +12,41 @@ public class Balance {
         this.bal = 0;
     }
 
-    public Balance(int b) {
+    /**
+     * Overload for constructor
+     * @param b: the starting balance
+     */
+    public Balance(double b) {
         this.bal = b;
+    }
+
+    /**
+     * Adds an amount to the balance
+     * @param b: the amount to be added
+     */
+    public void addBal(double b) {
+        this.bal += b;
+    }
+
+    /**
+     * Removes a given amount from the balance
+     * @param b: the amount to be removed
+     * @return: returns true if the amount is successfully removed (balance isn't negative)
+     */
+    public boolean removeBal(double b) {
+        if (b <= this.bal) {
+            this.bal -= b;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * returns the balance
+     * @return: the current balance
+     */
+    public double getBal() {
+        return this.bal;
     }
 }
