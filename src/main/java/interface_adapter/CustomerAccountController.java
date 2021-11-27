fef222813cc4d4cfc1e2bfd6edd5e28133171713
Zhaoyu Yan <zhaoyu.yan@mail.utoronto.ca>
2021-11-27 18:13:02 -0500

    Add a function for checking account existence.

--- src/main/java/interface_adapter/CustomerAccountController.java
+++ src/main/java/interface_adapter/CustomerAccountController.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 public class CustomerAccountController {
 
     private final CustomerDataAccess db;
-    private ArrayList<CustomerAccount> accounts = new ArrayList<>();
+    private final ArrayList<CustomerAccount> accounts;
 
 
     /**
@@ -54,17 +54,13 @@ public class CustomerAccountController {
     }
 
 
-
-    //TODO
-    public boolean exists() {
-        return true;
+    public boolean exists(String username) {
+        for (CustomerAccount a : this.accounts){
+            if (a.getUsername().equals(username)){
+                return true;
+            }
+        }
+        return false;
     }
-
-
-    //TODO change the color preference of an account
-    public void changeColor(String name, String color) {}
-
-    //TODO implement method for change username, Pin, add balance, change color preference, color preference has
-    // three options, the default colors, allBlack, and Reverse.
 
 }
