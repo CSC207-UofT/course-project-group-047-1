88428569458e983f751b13601bc5ea5be5c191e7
Zhaoyu Yan <zhaoyu.yan@mail.utoronto.ca>
2021-11-27 18:43:27 -0500

    Implement InventoryPresenter. Add a view method that displays the string representation of grocery inventory.

--- src/main/java/interface_adapter/InventoryPresenter.java
+++ src/main/java/interface_adapter/InventoryPresenter.java
@@ -1,4 +1,37 @@
 package interface_adapter;
 
+import entity.GroceryItem;
+import use_case.InventoryDataAccess;
+
+import java.util.ArrayList;
+
 public class InventoryPresenter {
+
+    InventoryDataAccess db;
+
+    ArrayList<GroceryItem> items;
+
+    public InventoryPresenter(InventoryDataAccess db) {
+        this.db = db;
+        this.items = db.read();
+    }
+
+
+    /**
+     * @return a string representation of GroceryItems in the store
+     */
+    public String view() {
+
+        StringBuilder string = new StringBuilder();
+
+        for (GroceryItem item : this.items) {
+
+            string.append(item.getId()).append(" ").append(item.getName()).append(" x ").append(item.getQuantity())
+                    .append(", Price: ").append(item.getPrice()).append("$ each\n");
+
+        }
+
+        return string.toString();
+
+    }
 }
