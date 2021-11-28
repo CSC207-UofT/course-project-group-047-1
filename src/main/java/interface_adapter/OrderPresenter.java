1aecaef84b57b2afa0892fcd4615ca0b7df6af0e
Zhaoyu Yan <zhaoyu.yan@mail.utoronto.ca>
2021-11-27 19:00:00 -0500

    Implement OrderPresenter. Add a view function that displays a string representation of all orders purchased by an account.

--- src/main/java/interface_adapter/OrderPresenter.java
+++ src/main/java/interface_adapter/OrderPresenter.java
@@ -13,17 +13,20 @@ public class OrderPresenter {
 
     public OrderPresenter(OrderDataAccess db) {
         this.db = db;
-        this.orders = this.db.read();
+        this.orders = db.read();
     }
 
-    //TODO this method returns a string representation of all orders
-    public String view() {
-        return "";
-    }
 
-    //TODO this method returns a string representation of order by a specific customer
     public String view(String name) {
-        return "";
+
+        StringBuilder str = new StringBuilder();
+
+        for (Order o : this.orders){
+            if (o.getCustomer().equals(name)){
+                str.append(o.returnInfo());
+            }
+        }
+        return str.toString();
     }
 
 }
