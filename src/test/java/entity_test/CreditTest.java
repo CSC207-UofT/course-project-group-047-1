617d6ec3f1e4b3a601da859ed97985d04edb67bc
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-29 21:49:05 -0500

    added testing file for Credit and the Before

---
+++ src/test/java/entity_test/CreditTest.java
@@ -0,0 +1,15 @@
+package entity_test;
+
+import entity.Credit;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CreditTest {
+    Credit cred;
+
+    @Before
+    public void setUp() {cred = new Credit();}
+
+
+}
