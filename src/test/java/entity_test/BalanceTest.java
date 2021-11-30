07c6ad6389ac321235a5739b574569a9802a6f5e
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-29 21:24:26 -0500

    Added test file for Balance and before setup

---
+++ src/test/java/entity_test/BalanceTest.java
@@ -0,0 +1,14 @@
+package entity_test;
+
+import entity.Balance;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BalanceTest {
+    Balance bal;
+
+    @Before
+    public void setUp() {bal = new Balance();}
+
+
+}
