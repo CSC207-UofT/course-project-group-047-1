41e29220e6ed57af1fa20cc4f349b79860512377
wenzhenw <wenzhen.wang@mail.utoronto.ca>
2021-11-29 21:31:55 -0500

    added testGetBal in BalanceTest.java

--- src/test/java/entity_test/BalanceTest.java
+++ src/test/java/entity_test/BalanceTest.java
@@ -3,12 +3,22 @@ package entity_test;
 import entity.Balance;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class BalanceTest {
-    Balance bal;
+    Balance bal1;
+    Balance bal2;
 
     @Before
-    public void setUp() {bal = new Balance();}
+    public void setUp() {
+        bal1 = new Balance();
+        bal2 = new Balance(2);
+    }
 
+    @Test(timeout = 50)
+    public void testGetBal() {
+        assertEquals(0.0, bal1.getBal(), 0.0);
+        assertEquals(2.0, bal2.getBal(), 0.0);
+    }
 
 }
