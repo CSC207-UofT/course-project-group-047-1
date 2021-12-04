ffb3150e922d506a30cd4ef76130984b180e5bb1
candicelu <candicecl.lu@mail.utoronto.ca>
2021-12-04 15:06:45 -0500

    Added description for Interfaces in use_case package

--- src/main/java/use_case/OrderDataAccess.java
+++ src/main/java/use_case/OrderDataAccess.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 
 public interface OrderDataAccess {
 
+        /**
+         * @return Read Orders.txt and return an ArrayList of Order
+         */
         ArrayList<Order> read();
 
+        /**
+         * @param orders
+         * update Orders.txt according to changes in orders
+         */
         void update(ArrayList<Order> orders);
 }
